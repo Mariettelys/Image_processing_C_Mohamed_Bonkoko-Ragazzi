aa09bc2ec27fde8117a794754259491abbc85b7a
Mariettelys <mariette.bonkoko--ragazzi@efrei.net>
2025-04-27 22:35:07 +0200

    Partie 1

--- main.c
+++ main.c
@@ -4,19 +4,19 @@
 
 int main() {
 
-     // fonction 2.1 bmp8_LoadImage
-     t_bmp8 *img = bmp8_loadImage("lena_gray.bmp");
+    // fonction 2.1 bmp8_LoadImage
+    t_bmp8 *img = bmp8_loadImage("lena_gray.bmp");
 
-     // fonction 2.3 bmp8_free
-     bmp8_free(img);
+    // fonction 2.3 bmp8_free
+    bmp8_free(img);
 
-     // fonction 3.1 bmp8_negative
-     bmp8_negative(img);
+    // fonction 3.1 bmp8_negative
+    bmp8_negative(img);
 
-     // fonction 3.3 bmp8_threshold
-     bmp8_threshold(img);
+    // fonction 3.3 bmp8_threshold
+    bmp8_threshold(img);
 
-     // fonction 4.1
-     // bmp8_applyFilter()
-     return 0;
+    // fonction 4.1
+    // bmp8_applyFilter()
+    return 0;
 }
