359bd3a5a864fc495e0eea1c69270ac9002d304e
Mariettelys <mariette.bonkoko--ragazzi@efrei.net>
2025-04-27 21:06:48 +0200

    commit partie 1 fonction

--- bmp8.h
+++ bmp8.h
@@ -1,11 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 
 typedef struct {
 unsigned char header[54]; /* En tête */
 unsigned char colorTable[1024]; /* table de couleur */
-unsigned char * data; /*données de l'image sous fprme de tableau*/
+unsigned char * data; /*données de l'image sous forme de tableau*/
 unsigned int width; /* largeur de l'image en pixels (offset 18) */
 unsigned int height; /* hauteur de l'image en pixels (offset 22)*/
 unsigned int colorDepth ; /* profondeur de couleur de l'image (8)*/
 unsigned int dataSize; /* taille des données de l'image en octets (offset 28 )*/
 
 } t_bmp8;
+
+t_bmp8 * bmp8_loadImage( const char * filename) ;
+
+void bmp8_free(t_bmp8 * img);
+
+void bmp8_negative(t_bmp8 * img);
+
+void bmp8_threshold(t_bmp8 * img);
+
+void bmp8_applyFilter(t_bmp8 * img, float ** kernel, int kernelSize);
--- main.c
+++ main.c
@@ -1,1 +1,22 @@
+#include "bmp8.h"
+#include <stdio.h>
+#include <stdlib.h>
 
+int main() {
+
+     // fonction 2.1 bmp8_LoadImage
+     t_bmp8 *img = bmp8_loadImage("lena_gray.bmp");
+
+     // fonction 2.3 bmp8_free
+     bmp8_free(img);
+
+     // fonction 3.1 bmp8_negative
+     bmp8_negative(img);
+
+     // fonction 3.3 bmp8_threshold
+     bmp8_threshold(img);
+
+     // fonction 4.1
+     // bmp8_applyFilter()
+     return 0;
+}
