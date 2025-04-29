9de8bcafac08cc860c07f4abc136f34c01e050f4
Mariettelys <mariette.bonkoko--ragazzi@efrei.net>
2025-04-29 12:11:29 +0200

    Partie 1

--- bmp8.h
+++ bmp8.h
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -10,21 +9,6 @@ unsigned int width; /* largeur de l'image en pixels (offset 18) */
 unsigned int height; /* hauteur de l'image en pixels (offset 22)*/
 unsigned int colorDepth ; /* profondeur de couleur de l'image (8)*/
 unsigned int dataSize; /* taille des données de l'image en octets (offset 28 )*/
-
-} t_bmp8;
-
-#include <stdio.h>
-#include <stdlib.h>
-
-
-typedef struct {
-    unsigned char header[54]; /* En tête */
-    unsigned char colorTable[1024]; /* table de couleur */
-    unsigned char * data; /*données de l'image sous forme de tableau*/
-    unsigned int width; /* largeur de l'image en pixels (offset 18) */
-    unsigned int height; /* hauteur de l'image en pixels (offset 22)*/
-    unsigned int colorDepth ; /* profondeur de couleur de l'image (8)*/
-    unsigned int dataSize; /* taille des données de l'image en octets (offset 28 )*/
 
 } t_bmp8;
 
