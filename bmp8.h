35fa73a28b182466b5da722833548dbff80fcca4
Mariettelys <mariette.bonkoko--ragazzi@efrei.net>
2025-05-25 22:49:29 +0200

    Update bmp8.h

--- bmp8.h
+++ bmp8.h
@@ -26,6 +26,20 @@ void menu_partie1_et_3_gris();
 void menu_partie2_et_3_couleur();
 
 // Partie 1 Image BMP 8 bits
+//Fonction qui permet de lire une image BMP en niveaux de gris
+t_bmp8 * bmp8_loadImage( const char * filename) ;
+
+// Fonction qui permet de libérer la mémoire allouée pour stocker une image de type t_bmp8
+void bmp8_free(t_bmp8 * img);
+
+// Fonction qui permet d'inverser les couleurs d'une image en niveaux de gris
+void bmp8_negative(t_bmp8 * img);
+
+// Fonction qui permet de transformer une image en niveaux de gris en une image binaire
+void bmp8_threshold(t_bmp8 * img);
+
+// Fonction qui applique un filtre sur une image en niveaux de gris
+void bmp8_applyFilter(t_bmp8 * img, float ** kernel, int kernelSize);
 
 int bmp8_saveImage( const char* filename, t_bmp8 * image);
 void bmp8_printInfo(t_bmp8 *image);
